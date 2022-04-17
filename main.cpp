2ecf40bce81deddd93f318ce5b71b87c98ff7364
Илья Бузин <ilya18198@mail.ru>
2022-04-17 15:09:57 +0300

    task
    Created a new branch and added an iterative fib function.
    And added a bubble sorting function.

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int fib(int n) {
+    if (n == 1)
+        return 0;
+    if (n == 2)
+        return 1;
+
+    int a = 0;
+    int b = 1;
+    for (int i = 0; i < n - 1; ++i) {
+        int t = a;
+        a = b;
+        b = b + t;
+    }
+
+    return a;
+}
+
+int main()
+{
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+    cout << "Hello, world!" << endl;
+    cout << fib(10) << endl;
+}
